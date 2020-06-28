1e717150b7c7a3851959a8c7c80f7644cb9ae07a
Peter Wilson <wilson@peterwilson.cc>
2020-06-28 13:17:17 +1000

    Basic plugin structure.

---
+++ inc/namespace.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WP Populated Publish
+ *
+ * @package           wp-populated-publish
+ * @author            Peter Wilson
+ * @copyright         20202 Peter Wilson
+ * @license           GPL-2.0-or-later
+ */
+
+namespace PWCC\PopulatedPublish;
+
+/**
+ * Register hooks.
+ *
+ * Runs on the `plugins_loaded` action.
+ */
+function bootstrap() : void {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ plugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * WP Populated Publish
+ *
+ * @package           wp-populated-publish
+ * @author            Peter Wilson
+ * @copyright         20202 Peter Wilson
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Populated Publish
+ * Plugin URI:        https://peterwilson.cc/projects/wp-populated-publish
+ * Description:       Ensure save hooks run after all data is populated.
+ * Version:           0.1.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.0
+ * Author:            Peter Wilson
+ * Author URI:        https://peterwilson.cc
+ * Text Domain:       wp-populated-publish
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+namespace PWCC\PopulatedPublish;
+
+require_once __DIR__ . '/inc/namespace.php';
+
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
