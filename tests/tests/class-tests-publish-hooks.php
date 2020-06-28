5e74e36b078e7f26be7661fedefb54f3e2de05f2
Peter Wilson <wilson@peterwilson.cc>
2020-06-28 18:17:58 +1000

    Set up some random poasts.

--- tests/tests/class-tests-publish-hooks.php
+++ tests/tests/class-tests-publish-hooks.php
@@ -12,11 +12,29 @@
 namespace PWCC\PopulatedPublish\Tests;
 
 use WP_UnitTestCase;
+use WP_UnitTest_Factory;
 
 /**
  * Test Publishing hooks run as expected.
  */
 class Tests_Publish_Hooks extends WP_UnitTestCase {
+
+	/**
+	 * An array of posts.
+	 *
+	 * @var array
+	 */
+	static $posts;
+
+	/**
+	 * Create posts to use throughout the test suite.
+	 *
+	 * @param WP_UnitTest_Factory $factory Test suite factory.
+	 * @return void
+	 */
+	static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) : void {
+		self::$posts = $factory->post->create_many( 5 );
+	}
 
 	/**
 	 * Test the tests.
