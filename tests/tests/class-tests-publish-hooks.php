a628f34f15cef7c039fd954eaa1ee8faf06fb83d
Peter Wilson <wilson@peterwilson.cc>
2020-06-28 13:27:37 +1000

    A PHPUnit Config with a dummy test.

---
+++ tests/tests/class-tests-publish-hooks.php
@@ -0,0 +1,19 @@
+<?php
+namespace PWCC\PopulatedPublish\Tests;
+
+use WP_UnitTestCase;
+
+/**
+ * Test Publishing hooks run as expected.
+ */
+class Tests_Publish_Hooks extends WP_UnitTestCase {
+
+	/**
+	 * Test the tests.
+	 *
+	 * @return void
+	 */
+	function test_dummy() : void {
+		$this->assertTrue( true );
+	}
+}
